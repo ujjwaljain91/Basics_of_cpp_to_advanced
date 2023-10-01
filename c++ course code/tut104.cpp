133f74fcc4f5ff34d7b3d75e2c9ea8299bc93186
Ujjwal Jain <145676688+ujjwaljain91@users.noreply.github.com>
2023-10-01 09:25:51 +0530

    Add files via upload
    new advanced level questions

---
+++ c++ course code/tut104.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout << "Enter  a number =";
+    cin >> n;
+    for (row = n; row >= 1; row = row - 1)
+    {
+        for (col = 1; col <=row; col = col + 1)
+
+            cout << "*";
+
+        for (col = 1; col <= 2 *n-2* row; col = col + 1)
+
+            cout << " ";
+             for (col = 1; col <=row; col = col + 1)
+
+            cout <<"*";
+
+        cout << endl;
+    }
+
+    for (row = 1; row<=n; row = row + 1)
+    {
+        for (col = 1; col <=row; col = col + 1)
+
+            cout << "*";
+
+        for (col = 1; col <= 2 *n-2* row; col = col + 1)
+
+            cout << " ";
+             for (col = 1; col <=row; col = col + 1)
+
+            cout <<"*";
+            cout<<endl;
+    }
+}
